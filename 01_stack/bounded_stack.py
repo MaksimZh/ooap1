5bf7891962bcf870e1a55bf9380744e28f04059a
Maksim Zholudev <maximzms@gmail.com>
2022-12-14 23:01:25 +0300

    Finish BoundedStack implementation

--- 01_stack/bounded_stack.py
+++ 01_stack/bounded_stack.py
@@ -2,31 +2,58 @@ from typing import Any
 
 class BoundedStack:
 
+    __stack: list[Any]
+    __max_size: int
+    __push_status: int
     __peek_status: int
     __pop_status: int
 
     PUSH_NIL: int = 0
+    PUSH_OK: int = 1
+    PUSH_ERR: int = 2
     POP_NIL: int = 0
+    POP_OK: int = 1
     POP_ERR: int = 2
     PEEK_NIL: int = 0
+    PEEK_OK: int = 1
     PEEK_ERR: int = 2
 
     def __init__(self, max_size: int = 32):
-        self.__pop_status = self.POP_NIL
-        self.__peek_status = self.PEEK_NIL
+        self.clear()
+        self.__max_size = max_size
+
+    def push(self, value: Any) -> None:
+        if self.size() == self.__max_size:
+            self.__push_status = self.PUSH_ERR
+            return
+        self.__stack.append(value)
+        self.__push_status = self.PUSH_OK
 
     def pop(self) -> None:
-        self.__pop_status = self.POP_ERR
+        if self.size() == 0:
+            self.__pop_status = self.POP_ERR
+            return
+        self.__stack.pop()
+        self.__pop_status = self.POP_OK
 
+    def clear(self) -> None:
+        self.__stack = []
+        self.__push_status = self.PUSH_NIL
+        self.__pop_status = self.POP_NIL
+        self.__peek_status = self.PEEK_NIL
+    
     def peek(self) -> Any:
-        self.__peek_status = self.PEEK_ERR
-        return 0
+        if self.size() == 0:
+            self.__peek_status = self.PEEK_ERR
+            return 0
+        self.__peek_status = self.PEEK_OK
+        return self.__stack[-1]
 
     def size(self) -> int:
-        return 0
+        return len(self.__stack)
 
     def get_push_status(self) -> int:
-        return self.PUSH_NIL
+        return self.__push_status
 
     def get_pop_status(self) -> int:
         return self.__pop_status
