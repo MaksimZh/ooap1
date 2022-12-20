61a90f416fe7cab432340395389a14626a6960bc
Maksim Zholudev <maximzms@gmail.com>
2022-12-20 16:27:11 +0300

    Add init test

--- 03_two_way_list/lists.py
+++ 03_two_way_list/lists.py
@@ -1,7 +1,92 @@
+from enum import Enum
 from abc import ABC
 
 class ParentList(ABC):
-    pass
+
+    class HeadStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_head_status(self) -> HeadStatus:
+        return self.HeadStatus.NIL
+    
+    class TailStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_tail_status(self) -> TailStatus:
+        return self.TailStatus.NIL
+
+    class RightStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        NO_RIGHT_NEIGHBOR = 2,
+
+    def get_right_status(self) -> RightStatus:
+        return self.RightStatus.NIL
+
+    class PutRightStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_put_right_status(self) -> PutRightStatus:
+        return self.PutRightStatus.NIL
+
+    class PutLeftStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_put_left_status(self) -> PutLeftStatus:
+        return self.PutLeftStatus.NIL
+
+    class RemoveStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_remove_status(self) -> RemoveStatus:
+        return self.RemoveStatus.NIL
+
+    class ReplaceStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_replace_status(self) -> ReplaceStatus:
+        return self.ReplaceStatus.NIL
+
+    class FindStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_find_status(self) -> FindStatus:
+        return self.FindStatus.NIL
+
+    class GetStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        EMPTY = 2,
+
+    def get_get_status(self) -> GetStatus:
+        return self.GetStatus.NIL
+
+    def size(self) -> int:
+        return 0
+
+    def is_head(self) -> bool:
+        return False
+
+    def is_tail(self) -> bool:
+        return False
+
+    def is_value(self) -> bool:
+        return False
+
 
 class LinkedList(ParentList):
     pass
--- 03_two_way_list/test_lists.py
+++ 03_two_way_list/test_lists.py
@@ -1,12 +1,26 @@
 import unittest
+from typing import Type
 
-from lists import LinkedList, TwoWayList
+from lists import ParentList, LinkedList, TwoWayList
 
 class Test_List(unittest.TestCase):
-    List = None
+    List: Type[ParentList]
 
-    def test(self):
-        print(self.List)
+    def test_init(self):
+        ls: ParentList = self.List()
+        self.assertFalse(ls.is_head())
+        self.assertFalse(ls.is_tail())
+        self.assertFalse(ls.is_value())
+        self.assertEqual(ls.size(), 0)
+        self.assertEqual(ls.get_head_status(), self.List.HeadStatus.NIL)
+        self.assertEqual(ls.get_tail_status(), self.List.TailStatus.NIL)
+        self.assertEqual(ls.get_right_status(), self.List.RightStatus.NIL)
+        self.assertEqual(ls.get_put_right_status(), self.List.PutRightStatus.NIL)
+        self.assertEqual(ls.get_put_left_status(), self.List.PutLeftStatus.NIL)
+        self.assertEqual(ls.get_remove_status(), self.List.RemoveStatus.NIL)
+        self.assertEqual(ls.get_replace_status(), self.List.ReplaceStatus.NIL)
+        self.assertEqual(ls.get_find_status(), self.List.FindStatus.NIL)
+        self.assertEqual(ls.get_get_status(), self.List.GetStatus.NIL)
 
 
 class Test_LinkedList(Test_List):
