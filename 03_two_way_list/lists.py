fa96dcb126d1b4066444685501e0e1753ae267f8
Maksim Zholudev <maximzms@gmail.com>
2022-12-20 18:51:40 +0300

    Finish first test case for lists

--- 03_two_way_list/lists.py
+++ 03_two_way_list/lists.py
@@ -1,82 +1,152 @@
 from enum import Enum
 from abc import ABC
+from typing import Any
+
 
 class ParentList(ABC):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.clear()
+
+
+    def head(self) -> None:
+        self.__head_status = self.HeadStatus.EMPTY
 
     class HeadStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
+    __head_status: HeadStatus
+
     def get_head_status(self) -> HeadStatus:
-        return self.HeadStatus.NIL
-    
+        return self.__head_status
+
+
+    def tail(self) -> None:
+        self.__tail_status = self.TailStatus.EMPTY
+
     class TailStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
+    __tail_status: TailStatus
+
     def get_tail_status(self) -> TailStatus:
-        return self.TailStatus.NIL
+        return self.__tail_status
+
+
+    def right(self) -> None:
+        self.__right_status = self.RightStatus.NO_RIGHT_NEIGHBOR
 
     class RightStatus(Enum):
         NIL = 0,
         OK = 1,
         NO_RIGHT_NEIGHBOR = 2,
+    
+    __right_status: RightStatus
 
     def get_right_status(self) -> RightStatus:
-        return self.RightStatus.NIL
+        return self.__right_status
+
+
+    def put_right(self, value: Any) -> None:
+        self.__put_right_status = self.PutRightStatus.EMPTY
 
     class PutRightStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
+    __put_right_status: PutRightStatus
+
     def get_put_right_status(self) -> PutRightStatus:
-        return self.PutRightStatus.NIL
+        return self.__put_right_status
+
+
+    def put_left(self, value: Any) -> None:
+        self.__put_left_status = self.PutLeftStatus.EMPTY
 
     class PutLeftStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
+    __put_left_status: PutLeftStatus
+
     def get_put_left_status(self) -> PutLeftStatus:
-        return self.PutLeftStatus.NIL
+        return self.__put_left_status
+
+
+    def remove(self) -> None:
+        self.__remove_status = self.RemoveStatus.EMPTY
 
     class RemoveStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
+    __remove_status: RemoveStatus
+
     def get_remove_status(self) -> RemoveStatus:
-        return self.RemoveStatus.NIL
+        return self.__remove_status
+
+
+    def replace(self, value: Any) -> None:
+        self.__replace_status = self.ReplaceStatus.EMPTY
 
     class ReplaceStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
+    __replace_status: ReplaceStatus
+
     def get_replace_status(self) -> ReplaceStatus:
-        return self.ReplaceStatus.NIL
+        return self.__replace_status
+
+
+    def find(self, value: Any) -> None:
+        self.__find_status = self.FindStatus.NOT_FOUND
 
     class FindStatus(Enum):
         NIL = 0,
         OK = 1,
-        EMPTY = 2,
+        NOT_FOUND = 2,
+
+    __find_status: FindStatus
 
     def get_find_status(self) -> FindStatus:
-        return self.FindStatus.NIL
+        return self.__find_status
+
+
+    def get(self) -> Any:
+        self.__get_status = self.GetStatus.EMPTY
 
     class GetStatus(Enum):
         NIL = 0,
         OK = 1,
         EMPTY = 2,
 
-    def get_get_status(self) -> GetStatus:
-        return self.GetStatus.NIL
+    __get_status: GetStatus
 
-    def size(self) -> int:
-        return 0
+    def get_get_status(self) -> GetStatus:
+        return self.__get_status
+
+
+    def clear(self) -> None:
+        self.__head_status = self.HeadStatus.NIL
+        self.__tail_status = self.TailStatus.NIL
+        self.__right_status = self.RightStatus.NIL
+        self.__put_right_status = self.PutRightStatus.NIL
+        self.__put_left_status = self.PutLeftStatus.NIL
+        self.__remove_status = self.RemoveStatus.NIL
+        self.__replace_status = self.ReplaceStatus.NIL
+        self.__find_status = self.FindStatus.NIL
+        self.__get_status = self.GetStatus.NIL
+
 
     def is_head(self) -> bool:
         return False
@@ -87,9 +157,29 @@ class ParentList(ABC):
     def is_value(self) -> bool:
         return False
 
+    def size(self) -> int:
+        return 0
+
 
 class LinkedList(ParentList):
     pass
 
 class TwoWayList(ParentList):
-    pass
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.__left_status = self.LeftStatus.NIL
+
+
+    def left(self) -> None:
+        self.__left_status = self.LeftStatus.NO_LEFT_NEIGHBOR
+
+    class LeftStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        NO_LEFT_NEIGHBOR = 2,
+
+    __left_status: LeftStatus
+
+    def get_left_status(self) -> LeftStatus:
+        return self.__left_status
--- 03_two_way_list/test_lists.py
+++ 03_two_way_list/test_lists.py
@@ -21,14 +21,38 @@ class Test_List(unittest.TestCase):
         self.assertEqual(ls.get_replace_status(), self.List.ReplaceStatus.NIL)
         self.assertEqual(ls.get_find_status(), self.List.FindStatus.NIL)
         self.assertEqual(ls.get_get_status(), self.List.GetStatus.NIL)
+        ls.head()
+        self.assertEqual(ls.get_head_status(), self.List.HeadStatus.EMPTY)
+        ls.tail()
+        self.assertEqual(ls.get_tail_status(), self.List.TailStatus.EMPTY)
+        ls.right()
+        self.assertEqual(ls.get_right_status(), self.List.RightStatus.NO_RIGHT_NEIGHBOR)
+        ls.put_right(1)
+        self.assertEqual(ls.get_put_right_status(), self.List.PutRightStatus.EMPTY)
+        ls.put_left(1)
+        self.assertEqual(ls.get_put_left_status(), self.List.PutLeftStatus.EMPTY)
+        ls.remove()
+        self.assertEqual(ls.get_remove_status(), self.List.RemoveStatus.EMPTY)
+        ls.replace(1)
+        self.assertEqual(ls.get_replace_status(), self.List.ReplaceStatus.EMPTY)
+        ls.find(1)
+        self.assertEqual(ls.get_find_status(), self.List.FindStatus.NOT_FOUND)
+        _ = ls.get()
+        self.assertEqual(ls.get_get_status(), self.List.GetStatus.EMPTY)
 
 
 class Test_LinkedList(Test_List):
-    List = LinkedList
+    List: Type[LinkedList] = LinkedList
 
 
 class Test_TwoWayList(Test_List):
-    List = TwoWayList
+    List: Type[TwoWayList] = TwoWayList
+
+    def test_init_ext(self):
+        ls: TwoWayList = self.List()
+        self.assertEqual(ls.get_left_status(), self.List.LeftStatus.NIL)
+        ls.left()
+        self.assertEqual(ls.get_left_status(), self.List.LeftStatus.NO_LEFT_NEIGHBOR)
 
 
 del Test_List
