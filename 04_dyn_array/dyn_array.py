b302c80f37ecbfb74935db93475043cc61d5e212
Maksim Zholudev <maximzms@gmail.com>
2022-12-23 11:44:31 +0300

    Finished ADT for dynamic array

--- 04_dyn_array/dyn_array.py
+++ 04_dyn_array/dyn_array.py
@@ -1,4 +1,5 @@
 from typing import Any
+from enum import Enum
 
 class DynArray:
     
@@ -21,6 +22,7 @@ class DynArray:
     def append(self, item: Any) -> None:
         pass
 
+    
     # предусловие: значение индекса больше либо равно нулю и меньше либо равно размеру массива
     # постусловие: в позицию с заданным индексом вставлено новое значение,
     #              все последующие элементы сдвинуты на одну позицию вверх
@@ -28,18 +30,39 @@ class DynArray:
     def insert(self, item: Any, index: int) -> None:
         pass
 
+    class InsertStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        INDEX_OUT_OF_RANGE = 2,
+
+    def get_insert_status(self) -> InsertStatus:
+        return self.InsertStatus.NIL
+
+    
     # предусловие: значение индекса больше или равно нулю и меньше размера массива
     # постусловие: из позиции с заданным индексом удалено значение,
     #              все последующие элементы сдвинуты на одну позицию вниз
     # постусловие: размер буфера не превышает размер массива более чем в 2 раза
     def remove(self, index: int) -> None:
         pass
+
+    class RemoveStatus(Enum):
+        NIL = 0,
+        OK = 1,
+        INDEX_OUT_OF_RANGE = 2,
+
+    def get_remove_status(self) -> RemoveStatus:
+        return self.RemoveStatus.NIL
     
     
     # запросы
 
+    def get_count(self) -> int:
+        return 0
+
+    def get_capacity(self) -> int:
+        return 0
+
     # предусловие: значение индекса больше или равно нулю и меньше размера массива
     def get_item(self, index: int) -> Any:
         pass
-
-
