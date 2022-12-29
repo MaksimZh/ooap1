9f4b77c1ba7f1c7c0098e457976b92b68a35a490
Maksim Zholudev <maximzms@gmail.com>
2022-12-29 15:07:55 +0300

    Add buffer and hash iterator

--- 07_hash_table/hash_table.py
+++ 07_hash_table/hash_table.py
@@ -1,28 +1,26 @@
-from typing import Any
+from typing import Any, Callable
 from enum import Enum, auto
+from random import randrange
 
 
 class Buffer:
 
-    class __Empty:
-        pass
-
+    class __Empty: pass
     __empty_cell = __Empty()
-
-    class __Deleted:
-        pass
-
+    class __Deleted: pass
     __deleted_cell = __Deleted()
 
     __data: list[Any]
     __count: int
-    
+
+
     # конструктор
     # постусловие: создан новый пустой буфер заданного размера
-    def __init__(self, capacity: int):
+    def __init__(self, capacity: int) -> None:
         self.__data = [self.__empty_cell] * capacity
         self.__count = 0
         self.__put_status = self.PutStatus.NIL
+        self.__delete_status = self.DeleteStatus.NIL
         self.__get_cell_state_status = self.GetCellStateStauts.NIL
     
     
@@ -32,10 +30,7 @@ class Buffer:
     # предусловие: индекс лежит в допустимом диапазоне
     # предусловие: ячейка не содержит значение
     def put(self, index: int, value: Any) -> None:
-        if index < 0:
-            self.__put_status = self.PutStatus.INDEX_OUT_OF_RANGE
-            return
-        if index >= self.get_capacity():
+        if self.__is_index_out_of_range(index):
             self.__put_status = self.PutStatus.INDEX_OUT_OF_RANGE
             return
         if self.get_cell_state(index) == self.CellState.VALUE:
@@ -46,15 +41,40 @@ class Buffer:
         self.__put_status = self.PutStatus.OK
 
     class PutStatus(Enum):
-        NIL = auto(),
-        OK = auto(),
-        INDEX_OUT_OF_RANGE = auto(),
-        ALREADY_HAS_VALUE = auto(),
+        NIL = auto(),                # команда не вызывалась
+        OK = auto(),                 # успех
+        INDEX_OUT_OF_RANGE = auto(), # индекс за границами буфера
+        ALREADY_HAS_VALUE = auto(),  # в ячейке уже есть значение
 
     __put_status: PutStatus
 
     def get_put_status(self) -> PutStatus:
         return self.__put_status
+
+    
+    # удалить значение из указанной ячейки
+    # предусловие: индекс лежит в допустимом диапазоне
+    # предусловие: ячейка содержит значение
+    def delete(self, index: int) -> None:
+        if self.__is_index_out_of_range(index):
+            self.__delete_status = self.DeleteStatus.INDEX_OUT_OF_RANGE
+            return
+        if self.get_cell_state(index) != self.CellState.VALUE:
+            self.__delete_status = self.DeleteStatus.NO_VALUE
+            return
+        self.__data[index] = self.__deleted_cell
+        self.__delete_status = self.DeleteStatus.OK
+
+    class DeleteStatus(Enum):
+        NIL = auto(),                # команда не вызывалась
+        OK = auto(),                 # успех
+        INDEX_OUT_OF_RANGE = auto(), # индекс за границами буфера
+        NO_VALUE = auto(),           # в ячейке нет значения
+
+    __delete_status: DeleteStatus
+
+    def get_delete_status(self) -> DeleteStatus:
+        return self.__delete_status
 
     
     # запросы
@@ -76,6 +96,11 @@ class Buffer:
     # получить состояние указанной ячейки
     # предусловие: индекс лежит в допустимом диапазоне
     def get_cell_state(self, index: int) -> CellState:
+        if self.__is_index_out_of_range(index):
+            self.__get_cell_state_status = \
+                self.GetCellStateStauts.INDEX_OUT_OF_RANGE
+            return self.CellState.EMPTY
+        self.__get_cell_state_status = self.GetCellStateStauts.OK
         if self.__data[index] is self.__empty_cell:
             return self.CellState.EMPTY
         if self.__data[index] is self.__deleted_cell:
@@ -92,6 +117,7 @@ class Buffer:
     def get_get_cell_state_status(self) -> GetCellStateStauts:
         return self.__get_cell_state_status
 
+
     # получить значение из указанной ячейки
     # предусловие: индекс лежит в допустимом диапазоне
     # предусловие: ячейка содержит значение
@@ -99,16 +125,118 @@ class Buffer:
         return self.__data[index]
 
 
-class HashBuffer(Buffer):
-    pass
+    def __is_index_out_of_range(self, index: int) -> bool:
+        return index < 0 or index >= self.get_capacity()
+
+
+class HashIterator:
+
+    __size: int
+    __limit: int
+    __started: bool
+    __index: int
+    __step: int
+    __count: int
+    __h1: Callable[[int], int]
+    __h2: Callable[[int], int]
+
+    
+    # конструктор
+    # предусловия: лимит индексов не превосходит размер
+    # постусловие: создан новый итератор
+    def __init__(self, size: int, limit: int) -> None:
+        assert(limit <= size)
+        self.__size = size
+        self.__limit = limit
+        self.__started = False
+        self.__h1 = _make_hash_func(size, size - 1)
+        self.__h2 = _make_hash_func(size, size + 1)
+        self.__get_index_status = self.GetIndexStatus.NIL
+        self.__next_status = self.NextStatus.NIL
+
+    
+    # команды
+
+    # сгенерировать первый индекс для заданного значения
+    # постусловия: итератор готов генерировать следующие индексы
+    # постусловия: счётчик индексов сброшен до единицы
+    def start(self, value: Any) -> None:
+        x = hash(value)
+        if x < 0:
+            x = -x
+        self.__started = True
+        self.__index = self.__h1(x)
+        self.__step = self.__h2(x)
+        self.__count = 0
+        if self.__step == 0:
+            self.__step = 1
+
+    # сгенерировать следующий индекс
+    # предусловия: первый индекс был сгенерирован
+    # предусловия: счётчик индексов не достиг лимита
+    # постусловия: сгенерирован новый индекс
+    def next(self) -> None:
+        if not self.__started:
+            self.__next_status = self.NextStatus.NOT_STARTED
+            return
+        self.__count += 1
+        if self.__count >= self.__limit:
+            self.__next_status = self.NextStatus.LIMIT_REACHED
+            return
+        self.__index = (self.__index + self.__step) % self.__size
+        self.__next_status = self.NextStatus.OK
+    
+    class NextStatus(Enum):
+        NIL = auto(),           # запрос не вызывался
+        OK = auto(),            # успех
+        NOT_STARTED = auto(),   # генерация индексов не начата
+        LIMIT_REACHED = auto(), # счётчик индексов достиг лимита
+
+    __next_status: NextStatus
+
+    def get_next_status(self) -> NextStatus:
+        return self.__next_status
+    
+    
+    # запросы
+
+    # получить текущий индекс
+    # предусловие: индекс был сгенерирован
+    # предусловия: счётчик индексов не достиг лимита
+    def get_index(self) -> int:
+        if not self.__started:
+            self.__get_index_status = self.GetIndexStatus.NOT_STARTED
+            return 0
+        if self.__count >= self.__limit:
+            self.__get_index_status = self.GetIndexStatus.LIMIT_REACHED
+            return 0
+        self.__get_index_status = self.GetIndexStatus.OK
+        return self.__index
+
+    class GetIndexStatus(Enum):
+        NIL = auto(),           # запрос не вызывался
+        OK = auto(),            # успех
+        NOT_STARTED = auto(),   # генерация индексов не начата
+        LIMIT_REACHED = auto(), # счётчик индексов достиг лимита
+
+    __get_index_status: GetIndexStatus
+
+    def get_get_index_status(self) -> GetIndexStatus:
+        return self.__get_index_status
+
+
+def _make_hash_func(size: int, p: int) -> Callable[[int], int]:
+    a = randrange(1, p)
+    b = randrange(0, p)
+    return lambda x: ((a * x + b) % p) % size    
 
 
 class HashTable:
 
-    __buffer: HashBuffer
+    __buffer: Buffer
     
-    def __init__(self, capacity: int):
-        self.__buffer = HashBuffer(capacity)
+    def __init__(self, capacity: int) -> None:
+        self.__buffer = Buffer(capacity)
         self.__remove_status = self.RemoveStatus.NIL
 
 
--- 07_hash_table/test_hash_table.py
+++ 07_hash_table/test_hash_table.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import Any
 
-from hash_table import HashTable, HashBuffer, Buffer
+from hash_table import HashTable, HashIterator, Buffer
 EMPTY = Buffer.CellState.EMPTY
 DELETED = Buffer.CellState.DELETED
 VALUE = Buffer.CellState.VALUE
@@ -13,21 +13,37 @@ class Test_Buffer(unittest.TestCase):
         self.assertEqual(buffer.get_capacity(), len(pattern))
         count: int = 0
         for i in range(len(pattern)):
+            cell_state = buffer.get_cell_state(i)
+            self.assertEqual(buffer.get_get_cell_state_status(),
+                Buffer.GetCellStateStauts.OK)
             if pattern[i] == EMPTY:
-                self.assertEqual(buffer.get_cell_state(i), EMPTY)
+                self.assertEqual(cell_state, EMPTY)
                 continue
             if pattern[i] == DELETED:
-                self.assertEqual(buffer.get_cell_state(i), DELETED)
+                self.assertEqual(cell_state, DELETED)
                 continue
-            self.assertEqual(buffer.get_cell_state(i), VALUE)
+            self.assertEqual(cell_state, VALUE)
             count += 1
             self.assertEqual(buffer.get(i), pattern[i])
         
     def test_empty(self):
         b = Buffer(5)
         self.assertEqual(b.get_put_status(), Buffer.PutStatus.NIL)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.NIL)
         self.assertEqual(b.get_get_cell_state_status(), Buffer.GetCellStateStauts.NIL)
         self.check(b, [EMPTY] * 5)
+        b.get_cell_state(-1)
+        self.assertEqual(b.get_get_cell_state_status(),
+            Buffer.GetCellStateStauts.INDEX_OUT_OF_RANGE)
+        b.get_cell_state(5)
+        self.assertEqual(b.get_get_cell_state_status(),
+            Buffer.GetCellStateStauts.INDEX_OUT_OF_RANGE)
+        b.get_cell_state(-42)
+        self.assertEqual(b.get_get_cell_state_status(),
+            Buffer.GetCellStateStauts.INDEX_OUT_OF_RANGE)
+        b.get_cell_state(42)
+        self.assertEqual(b.get_get_cell_state_status(),
+            Buffer.GetCellStateStauts.INDEX_OUT_OF_RANGE)
 
     def test_put(self):
         b = Buffer(5)
@@ -51,13 +67,76 @@ class Test_Buffer(unittest.TestCase):
         b.put(2, "z")
         self.assertEqual(b.get_put_status(), Buffer.PutStatus.ALREADY_HAS_VALUE)
         self.check(b, ["a", EMPTY, "b", EMPTY, "c"])
+        b.put(1, "d")
+        self.assertEqual(b.get_put_status(), Buffer.PutStatus.OK)
+        b.put(3, "e")
+        self.assertEqual(b.get_put_status(), Buffer.PutStatus.OK)
+        self.check(b, ["a", "d", "b", "e", "c"])
+
+    def test_delete(self):
+        b = Buffer(5)
+        b.put(0, "a")
+        b.put(2, "b")
+        b.put(4, "c")
+        self.check(b, ["a", EMPTY, "b", EMPTY, "c"])
+        b.delete(0)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.OK)
+        self.check(b, [DELETED, EMPTY, "b", EMPTY, "c"])
+        b.delete(2)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.OK)
+        self.check(b, [DELETED, EMPTY, DELETED, EMPTY, "c"])
+        b.delete(4)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.OK)
+        self.check(b, [DELETED, EMPTY, DELETED, EMPTY, DELETED])
+        b.delete(-1)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.INDEX_OUT_OF_RANGE)
+        b.delete(5)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.INDEX_OUT_OF_RANGE)
+        b.delete(-42)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.INDEX_OUT_OF_RANGE)
+        b.delete(42)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.INDEX_OUT_OF_RANGE)
+        b.delete(1)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.NO_VALUE)
+        b.delete(2)
+        self.assertEqual(b.get_delete_status(), Buffer.DeleteStatus.NO_VALUE)
 
 
-class Test_HashBuffer(unittest.TestCase):
+class Test_HashIterator(unittest.TestCase):
+
+    def check_once(self):
+        size = 17
+        limit = 12
+        hi = HashIterator(size, limit)
+        self.assertEqual(hi.get_get_index_status(), HashIterator.GetIndexStatus.NIL)
+        self.assertEqual(hi.get_next_status(), HashIterator.NextStatus.NIL)
+        hi.next()
+        self.assertEqual(hi.get_next_status(), HashIterator.NextStatus.NOT_STARTED)
+        hi.get_index()
+        self.assertEqual(hi.get_get_index_status(), HashIterator.GetIndexStatus.NOT_STARTED)
+        hi.start("a")
+        i = hi.get_index()
+        self.assertEqual(hi.get_get_index_status(), HashIterator.GetIndexStatus.OK)
+        self.assertGreaterEqual(i, 0)
+        self.assertLess(i, size)
+        indices: set[int] = set([i])
+        for _ in range(limit - 1):
+            hi.next()
+            self.assertEqual(hi.get_next_status(), HashIterator.NextStatus.OK)
+            i = hi.get_index()
+            self.assertEqual(hi.get_get_index_status(), HashIterator.GetIndexStatus.OK)
+            self.assertGreaterEqual(i, 0)
+            self.assertLess(i, size)
+            self.assertNotIn(i, indices)
+            indices.add(i)
+        hi.next()
+        self.assertEqual(hi.get_next_status(), HashIterator.NextStatus.LIMIT_REACHED)
+        hi.get_index()
+        self.assertEqual(hi.get_get_index_status(), HashIterator.GetIndexStatus.LIMIT_REACHED)
 
     def test(self):
-        b = HashBuffer(5)
-        self.assertEqual(b.get_count(), 0)
+        for _ in range(100):
+            self.check_once()
 
 
 class Test_HashTable(unittest.TestCase):
